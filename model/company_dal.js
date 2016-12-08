331e4cf6eed648108f41abe030a086b96379d50d
mhaderman <mhaderman@users.noreply.github.com>
2016-12-08 09:19:28 -0800

    Added if statement to prevent looping over a string.

--- model/company_dal.js
+++ model/company_dal.js
@@ -48,10 +48,18 @@ exports.insert = function(params, callback) {
         // NOTE THAT THERE IS ONLY ONE QUESTION MARK IN VALUES ?
         var query = 'INSERT INTO company_address (company_id, address_id) VALUES ?';
 
-        // TO BULK INSERT RECORDS WE CREATE A MULTIDIMENSIONAL ARRAY OF THE VALUES
+        // TO BULK INSERT AN ARRAY OF VALUES WE CREATE A MULTIDIMENSIONAL ARRAY OF THE VALUES        
         var companyAddressData = [];
-        for(var i=0; i < params.address_id.length; i++) {
-            companyAddressData.push([company_id, params.address_id[i]]);
+        // if only one value is submitted, JavaScript will treat the value as an array, so we skip it if its not an array
+        // for example if the value of params.address_id was "10", it would loop over the "1" and then the "0", instead of
+        // treating it as one value.
+        if(params.address_id instanceof Array) {  
+         for(var i=0; i < params.address_id.length; i++) {
+             companyAddressData.push([company_id, params.address_id[i]]);
+         }
+        }        
+        else {  
+         companyAddressData.push([company_id, params.address_id]);
         }
 
         // NOTE THE EXTRA [] AROUND companyAddressData
